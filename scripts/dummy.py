ecd4df9cb5758f37cd3c985adcaaeef4284f2153
Michael Mayrhofer <e12122564@student.tuwien.ac.at>
2025-09-10 14:06:32 +0200

    switch cnpg to helm

--- scripts/dummy.py
+++
@@ -1,8 +0,0 @@
-import mlflow
-from mlflow.tracking import MlflowClient
-
-mlflow.set_tracking_uri("http://127.0.0.1:5000")
-
-run_id = "e82f76cf944848ad8830cdd494f739ca"  # your last run
-run = MlflowClient().get_run(run_id)
-print("artifact_uri:", run.info.artifact_uri)
