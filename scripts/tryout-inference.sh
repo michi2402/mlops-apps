7015e54c6565efe38e8bc65122f2e41897575532
Michael Mayrhofer <michael_mayrhofer@outlook.com>
2025-09-24 17:40:56 +0200

    add gateway class

---
+++ scripts/tryout-inference.sh
@@ -0,0 +1,15 @@
+curl -s \
+  -H "Content-Type: application/json" \
+  -d '{
+    "inputs": [{
+      "name": "predict",
+      "shape": [3, 4],
+      "datatype": "FP64",
+      "data": [
+        [5.1, 3.5, 1.4, 0.2],
+        [6.2, 3.4, 5.4, 2.3],
+        [5.9, 3.0, 4.2, 1.5]
+      ]
+    }]
+  }' \
+  http://127.0.0.1:1111/v2/models/dummy-model/infer | jq .
